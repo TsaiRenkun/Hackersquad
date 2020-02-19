187fb5d3a66bb116941d6d731209463fd88450a6
leowzhenkang <leowzk@hotmail.com>
2020-02-19 15:25:57 +0800

    added view all events, add events, edit events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,29 +1,32 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, :except => [ :show, :index ]
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, :except => [ :show, :index ]
+  # before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
   def index
     @events = Event.all
-    @profile = current_user.profile
+    # @profile = current_user.profile
   end
 
   # GET /events/1
   # GET /events/1.json
   def show
-    @profile = current_user.profile
+    # @profile = current_user.profile
+    @event = Event.find(params[:id])
+
   end
 
   # GET /events/new
   def new
     @event = Event.new
-    @profile = current_user.profile
+    # @profile = current_user.profile
   end
 
   # GET /events/1/edit
   def edit
-    @profile = current_user.profile
+    # @profile = current_user.profile
+    @event = Event.find(params[:id])
   end
 
   # POST /events
@@ -33,29 +36,17 @@ class EventsController < ApplicationController
 
     @event.user = current_user
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    @event.save
+    redirect_to @event
   end
 
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    @event = Event.find(params[:id])
+
+    @event.update(event_params)
+    redirect_to @event
   end
 
   # DELETE /events/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,73 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #zk code start below
+  root 'events#index'
+  get '/events/new' => 'events#new', as: 'new_event'
+  post '/events' => 'events#create'
+  get '/events/:id' => 'events#show' , as: 'event'
+  get '/events/:id/edit' => 'events#edit', as: 'edit_event'
+  patch '/events/:id' => 'events#update'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#safs code start below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#kun code start below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
